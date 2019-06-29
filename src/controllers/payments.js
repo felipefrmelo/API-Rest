44416a8bf4151dcdbec101f94789f3c16d6a6687
felipefrmelo <felipefrmelo@gmail.com>
2019-06-29 20:36:11 -0300

    adicionado memcached

--- src/controllers/payments.js
+++ src/controllers/payments.js
@@ -11,6 +11,38 @@ module.exports = (app) => {
     res.send('OK.');
   });
 
+  app.get('/pagamentos/pagamento/:id', function (req, res) {
+    var id = req.params.id;
+    console.log('consultando pagamento: ' + id);
+
+    var memcachedClient = app.service.memcachedClient();
+
+    memcachedClient.get('pagamento-' + id, function (erro, retorno) {
+      if (erro || !retorno) {
+        console.log('MISS - chave nao encontrada');
+
+        var connection = app.persistencia.connectionFactory();
+        var pagamentoDao = new app.persistencia.PagamentoDao(connection);
+
+        pagamentoDao.buscaPorId(id, function (erro, resultado) {
+          if (erro) {
+            console.log('erro ao consultar no banco: ' + erro);
+            res.status(500).send(erro);
+            return;
+          }
+          console.log('pagamento encontrado: ' + JSON.stringify(resultado));
+          res.json(resultado);
+          return;
+        });
+        //HIT no cache
+      } else {
+        console.log('HIT - valor: ' + JSON.stringify(retorno));
+        res.json(retorno);
+        return;
+      }
+    });
+  });
+
   app.post("/pagamentos/pagamento", [
     check('pagamento.forma_de_pagamento').not().isEmail().withMessage('Forma de pagamento é obrigatória'),
     check('pagamento.valor').not().isEmpty().withMessage('Valor é obrigatório e deve ser um decimal.'),
@@ -23,7 +55,7 @@ module.exports = (app) => {
       return res.status(422).json({ errors: errors.array() });
     }
 
-    const { pagamento }  = req.body;
+    const { pagamento } = req.body;
     console.log('processando uma requisicao de um novo pagamento');
 
     pagamento.status = PAGAMENTO_CRIADO;
@@ -32,13 +64,18 @@ module.exports = (app) => {
     const connection = app.persistencia.connectionFactory();
     const pagamentoDao = new app.persistencia.PagamentoDao(connection);
 
-    pagamentoDao.salva(pagamento, function (erro, resultado) {
+    pagamentoDao.salva(pagamento, function (erro, result) {
       if (erro) {
         console.log('Erro ao inserir no banco:' + erro);
         res.status(500).send(erro);
       } else {
-        pagamento.id = resultado.insertId;
+        pagamento.id = result.insertId;
         console.log('pagamento criado');
+
+        const cache = app.service.memcachedClient();
+        cache.set('pagamento-' + pagamento.id, pagamento, 1000, function (err) {
+          console.log('nova chave: pagamento-' + pagamento.id)
+        });
 
         if (pagamento.forma_de_pagamento == 'cartao') {
           const cartao = req.body["cartao"];
@@ -110,46 +147,46 @@ module.exports = (app) => {
     });
   });
 
-app.put("/pagamentos/pagamento/:id", (req, res) => {
+  app.put("/pagamentos/pagamento/:id", (req, res) => {
 
-  const pagamento = {};
-  const id = req.params.id;
+    const pagamento = {};
+    const id = req.params.id;
 
-  pagamento.id = id;
-  pagamento.status = PAGAMENTO_CONFIRMADO;
+    pagamento.id = id;
+    pagamento.status = PAGAMENTO_CONFIRMADO;
 
-  const connection = app.persistencia.connectionFactory();
-  const pagamentoDao = new app.persistencia.PagamentoDao(connection);
+    const connection = app.persistencia.connectionFactory();
+    const pagamentoDao = new app.persistencia.PagamentoDao(connection);
 
-  pagamentoDao.atualiza(pagamento, function (erro) {
-    if (erro) {
-      res.status(500).send(erro);
-      return;
-    }
-    console.log('pagamento criado');
-    res.send(pagamento);
+    pagamentoDao.atualiza(pagamento, function (erro) {
+      if (erro) {
+        res.status(500).send(erro);
+        return;
+      }
+      console.log('pagamento criado');
+      res.send(pagamento);
+    });
   });
-});
 
-app.delete("/pagamentos/pagamento/:id", (req, res) => {
+  app.delete("/pagamentos/pagamento/:id", (req, res) => {
 
-  const pagamento = {};
-  const id = req.params.id;
+    const pagamento = {};
+    const id = req.params.id;
 
-  pagamento.id = id;
-  pagamento.status = PAGAMENTO_CANCELADO;
+    pagamento.id = id;
+    pagamento.status = PAGAMENTO_CANCELADO;
 
-  const connection = app.persistencia.connectionFactory();
-  const pagamentoDao = new app.persistencia.PagamentoDao(connection);
+    const connection = app.persistencia.connectionFactory();
+    const pagamentoDao = new app.persistencia.PagamentoDao(connection);
 
-  pagamentoDao.cancela(pagamento, function (erro) {
-    if (erro) {
-      res.status(500).send(erro);
-      return;
-    }
-    console.log('pagamento cancelado');
-    res.status(204).send(pagamento);
+    pagamentoDao.cancela(pagamento, function (erro) {
+      if (erro) {
+        res.status(500).send(erro);
+        return;
+      }
+      console.log('pagamento cancelado');
+      res.status(204).send(pagamento);
+    });
   });
-});
 
 }
