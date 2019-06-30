ca5933effb8472c4871d55101e0027b4a1f8c3ba
felipefrmelo <felipefrmelo@gmail.com>
2019-06-29 22:20:59 -0300

    add lib cluster

--- src/config/custom-express.js
+++ src/config/custom-express.js
@@ -2,10 +2,20 @@ const express = require('express');
 const consign = require('consign');
 const bodyParser = require('body-parser')
 const app = express();
+const morgan = require('morgan');
+const logger = require('../service/logger.js');
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json())
+
+app.use(morgan("common", {
+    stream: {
+      write: function(mensagem){
+          logger.info(mensagem);
+      }
+    }
+  }));
 
 
 consign({cwd: 'src'})
