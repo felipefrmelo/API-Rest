a2b73d464c0b4a85da40b7e0a00deacd85e1d09c
felipefrmelo <felipefrmelo@gmail.com>
2019-06-30 09:44:12 -0300

    add mysql

--- src/persistencia/connectionFactory.js
+++ src/persistencia/connectionFactory.js
@@ -2,9 +2,9 @@ var mysql  = require('mysql');
 
 function createDBConnection(){
         return mysql.createConnection({
-            host: 'localhost',
-            user: 'admin',
-            password: 'Fv123456@',
+            host: 'https://desolate-everglades-91879.herokuapp.com/',
+            user: 'b81ebd48c178a4',
+            password: '76bfaa29',
             database: 'payfast'
         });
 }
